5c8bc55d8ca985322c247a89b8c4619924fcadb3
落叶归根 <zxr10540116@outlook.com>
2017-05-15 14:03:02 +0800

    画图程序执行入口

---
+++ src/cad/CADApplication.java
@@ -0,0 +1,14 @@
+package cad;
+
+import static cad.util.SwingConsole.run;
+
+import cad.view.CadFrame;
+
+public class CADApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		run(new CadFrame("CAD画图工具"), 750, 610);
+	}
+
+}
