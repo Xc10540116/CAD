229903d4d1647b558c7d6a9905853567213165e7
落叶归根 <zxr10540116@outlook.com>
2017-05-16 17:01:01 +0800

    图形-直线
    根据图形基类的修改，修改直线

--- src/cad/model/Line.java
+++ src/cad/model/Line.java
@@ -2,17 +2,20 @@ package cad.model;
 
 import java.awt.BasicStroke;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.geom.Line2D;
 
 public class Line extends BaseShape {
 
 	private static final long serialVersionUID = -2028648010081272032L;
+	
+	private Point e_point;	// 绘制终点
 
 	@Override
 	public void draw(Graphics2D graphics) {
 		graphics.setColor(color);
 		graphics.setStroke(new BasicStroke(thick));
-		graphics.drawLine(b_point.x, b_point.y, e_point.x, e_point.y);
+		graphics.drawLine(point.x, point.y, e_point.x, e_point.y);
 	}
 
 	// TODO 点(x,y)是否在直线中
@@ -24,7 +27,7 @@ public class Line extends BaseShape {
 	@Override
 	public Boolean contains(int x, int y) {
 		// 生成直线对象
-		Line2D line = new Line2D.Float(b_point, e_point);
+		Line2D line = new Line2D.Float(point, e_point);
 		// 判断点到直线的距离
 		Double len2 = line.ptSegDist(x, y);
 		// 设置允许点击的距离
@@ -47,10 +50,7 @@ public class Line extends BaseShape {
 	 */
 	@Override
 	public void editShapeSize(int len) {
-		if(e_point.x - b_point.x == 1 && len < 0) {
-			System.out.println("已经最短了，再短就成点了！");
-			return ;
-		}
+		super.editShapeSize(len);
 		if(coordinate == 1) {
 			e_point.y = direction == 1 ? e_point.y + len : e_point.y - len;
 		} else if( coordinate == -1 ) {
@@ -63,13 +63,28 @@ public class Line extends BaseShape {
 
 	@Override
 	public void moveShape(int width, int height) {
-		b_point.translate(width, height);
+		super.moveShape(width, height);
 		e_point.translate(width, height);
 	}
 
-	@Override
-	public void removeShape() {
+	/**
+	 * 获取当前图形绘制的终点
+	 * @return 当前图形绘制的终点
+	 * @see BaseShape#setE_point(int, int)
+	 */
+	public Point getE_point() {
+		return e_point;
+	}
 
+	/**
+	 * 设置当前图形绘制的终点
+	 * @param x 坐标点x
+	 * @param y 坐标点y
+	 * @see BaseShape#getE_point()
+	 */
+	public void setE_point(int x, int y) {
+		e_point.x = x;
+		e_point.y = y;
 	}
 	
 	/**
@@ -80,7 +95,7 @@ public class Line extends BaseShape {
 	@Override
 	public String toString() {
 		String s = super.toString() + "\t";
-		s += "起点坐标" + b_point + ", 终点坐标" + e_point + ", ";
+		s += "起点坐标" + point + ", 终点坐标" + e_point + ", ";
 		s += "颜色: " + color + ", 粗细: " + thick;
 		s += ",coordinate：" + coordinate + ",direction: " + direction;
 		s += ",k=" + k + ",b=" + b;
