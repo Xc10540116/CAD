90b61a0925fb5dcbf709e4f816633c87364554b3
落叶归根 <zxr10540116@outlook.com>
2017-05-16 17:01:26 +0800

    图形-文字
    根据图形基类的修改，修改文字方法

--- src/cad/model/Words.java
+++ src/cad/model/Words.java
@@ -11,62 +11,33 @@ public class Words extends BaseShape {
 	private String str;	// 文字内容
 	
 	@Override
-	public void setE_point(int x, int y) {
-		// TODO Auto-generated method stub
-		super.setE_point(x, y);
-		int strlen = str.length();
-		if(width < strlen * height) {
-			height = width / strlen;
-		}
-		else
-			width = height * strlen;
-		setCoefficient(width, height);
-	}
-	
-	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(color);
 		int fontsize = (int)height;
-		setCoefficient(width, height);
 		g.setFont(new Font(null , Font.PLAIN, fontsize));
-		g.drawString(str, b_point.x, b_point.y);
+		g.drawString(str, point.x, point.y);
 	}
 
 	@Override
 	public Boolean contains(int x, int y) {
-		Rectangle2D rectange = new Rectangle2D.Double(b_point.x, b_point.y, width, height);
+		Rectangle2D rectange = new Rectangle2D.Double(point.x, point.y, width, height);
 		return rectange.contains(x, y);
 	}
-
+	
+	/**
+	 * toString方法复写了默认的toString方法，
+	 * 显示调用的类，内存地址，文字的基本信息（包含了
+	 * 绘制起点坐标、椭圆宽度、高度、颜色）。
+	 */
 	@Override
-	public void editShapeSize(int len) {
-		if(height == 1 && len < 0) {
-			System.out.println("已经最短了，再短就成点了！");
-			return ;
-		}
-		// 如果方程类型为x=?
-		if(coordinate == 1) {
-			// 高度为 （方向为正方向 ？ 高度+改变的大小 ： 高度-改变的大小）
-			height = direction == 1 ? height + len : height - len;
-		} else if( coordinate == -1 ) {	// 如果方程类型为y=?
-			// 宽度为 （方向为正方向 ？ 宽度+改变的大小 ： 宽度-改变的大小）
-			width = direction == 1 ? width + len : width - len;
-		} else {	// 方程类型为y=kx+b
-			width = width + len;
-			height = height + k * len;
-		}
-	}
-
-	@Override
-	public void moveShape(int width, int height) {
+	public String toString() {
 		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void removeShape() {
-		// TODO Auto-generated method stub
-
+		String s = super.toString() + "\t";
+		s += "文字绘制起点坐标" + point +  ",宽度：" + width;
+		s += ",高度：" + height + ",颜色: " + color;
+		s += ",coordinate：" + coordinate + ",direction: " + direction;
+		s += ",k=" + k + ",b=" + b;
+		return s;
 	}
 
 }
