c28964e8f338fc494e08dd4ebbb0a11e3248701c
落叶归根 <zxr10540116@outlook.com>
2017-05-16 16:57:00 +0800

    图形基类
    1.删除绘制终点；
    2.修改移动图形和改变图形大小的方法，由abstract改为普通方法；
    3.删除构造函数-传入颜色和粗细；
    4.新增构造函数-传入绘制起点、终点、颜色和粗细
    5.新增构造函数-传入绘制起点、颜色、粗细和长宽

--- src/cad/model/BaseShape.java
+++ src/cad/model/BaseShape.java
@@ -10,7 +10,6 @@ import java.awt.*;
  * 基本渲染操作所需的状态信息。 该状态信息包括以下属性： 
  * <ul>
  * <li>绘制起点坐标
- * <li>绘制终点坐标
  * <li>当前图形的颜色。
  * <li>当前图形的粗细。
  * <li>起点与终点水平方向的距离
@@ -31,8 +30,7 @@ public abstract class BaseShape implements java.io.Serializable {
 
 	private static final long serialVersionUID = 3023202635680952583L;
 
-	protected Point b_point = new Point();	// 绘制起点坐标
-	protected Point e_point = new Point();	// 绘制终点坐标
+	protected Point point = new Point();	// 绘制起点坐标
 	protected Color color = Color.BLACK;	// 颜色
 	protected float thick = 1.0f;			// 粗细
 	protected float width = 0.0f;			// 起点与终点水平方向的距离
@@ -66,61 +64,71 @@ public abstract class BaseShape implements java.io.Serializable {
 	}
 	
 	/**
-	 * 用指定的颜色和粗细构造并初始化一个基本图形
+	 * 用指定的绘制起点、颜色、粗细和长宽构造并初始化一个基本图形
+	 * @param x 起点坐标x
+	 * @param y 起点坐标y
 	 * @param color 颜色
 	 * @param thick 粗细
+	 * @param width 宽
+	 * @param height 长
 	 */
-	public BaseShape(Color color, Float thick) {
+	public BaseShape(int x, int y, Color color, float thick, float width, float height) {
+		super();
+		point.setLocation(x, y);
 		this.color = color;
 		this.thick = thick;
+		this.width = width;
+		this.height = height;
+		setCoefficient();
 	}
 	
 	/**
+	 * 用指定的绘制起点(x,y)、绘制终点(x1,y1)、颜色和粗细构建并初始化一个基本图形
+	 * @param x 绘制起点坐标x
+	 * @param y 绘制起点坐标y
+	 * @param x1 绘制终点坐标x1
+	 * @param y1 绘制终点坐标y1
+	 * @param color 颜色
+	 * @param thick 粗细
+	 */
+	public BaseShape(int x, int y, int x1, int y1, Color color, float thick) {
+		super();
+		point.setLocation(x, y);
+		this.color = color;
+		this.thick = thick;
+		int w = x1 - x;
+		int h = y1 - y;
+		width = (float)w;
+		height = (float)h;
+		if(width < 0) {
+			point.translate(w, 0);
+			width = Math.abs(width);
+		}
+		if(height < 0) {
+			point.translate(0, h);
+			height = Math.abs(height);
+		}
+		setCoefficient();
+	}
+
+	/**
 	 * 获取当前图形绘制的起始点
 	 * @return 当前图形绘制的起始点
-	 * @see BaseShape#setB_point(int, int)
+	 * @see BaseShape#setPoint(int, int)
 	 */
-	public Point getB_point() {
-		return b_point;
+	public Point getPoint() {
+		return point;
 	}
 
 	/**
 	 * 设置当前图形绘制的起始点
 	 * @param x 坐标点x
 	 * @param y 坐标点y
-	 * @see BaseShape#getB_point()
+	 * @see BaseShape#getPoint()
 	 */
-	public void setB_point(int x, int y) {
-		b_point.x = x;
-		b_point.y = y;
-	}
-
-	/**
-	 * 获取当前图形绘制的终点
-	 * @return 当前图形绘制的终点
-	 * @see BaseShape#setE_point(int, int)
-	 */
-	public Point getE_point() {
-		return e_point;
-	}
-
-	/**
-	 * 设置当前图形绘制的终点
-	 * @param x 坐标点x
-	 * @param y 坐标点y
-	 * @see BaseShape#getE_point()
-	 */
-	public void setE_point(int x, int y) {
-		e_point.x = x;
-		e_point.y = y;
-	}
-	
-	public void setLocation(int b_x, int b_y, int e_x, int e_y) {
-		b_point.x = b_x;
-		b_point.y = b_y;
-		e_point.x = e_x;
-		e_point.y = e_y;
-		setCoefficient();
+	public void setPoint(int x, int y) {
+		point.x = x;
+		point.y = y;
 	}
 
 	/**
@@ -143,28 +151,38 @@ public abstract class BaseShape implements java.io.Serializable {
 	 * 修改图形的大小
 	 * @param len 改变的值 - len为正，则图形增大；为负，则图形变小
 	 */
-	public abstract void editShapeSize(int len);
+	public void editShapeSize(int len) {
+		if((width <= 1 || height <= 1) && len < 0) {
+			System.out.println("已经最短了，再短就成点了！");
+			return ;
+		}
+		// 如果方程类型为x=?
+		if(coordinate == 1) {
+			// 高度为 （方向为正方向 ？ 高度+改变的大小 ： 高度-改变的大小）
+			height = direction == 1 ? height + len : height - len;
+		} else if( coordinate == -1 ) {	// 如果方程类型为y=?
+			// 宽度为 （方向为正方向 ？ 宽度+改变的大小 ： 宽度-改变的大小）
+			width = direction == 1 ? width + len : width - len;
+		} else {	// 方程类型为y=kx+b
+			width = width + len;
+			height = height + k * len;
+		}
+	}
 	
 	/**
 	 * 移动图形
 	 * @param width 位移宽度
 	 * @param height 位移高度
 	 */
-	public abstract void moveShape(int width, int height);
-	
-	public abstract void removeShape();
+	public void moveShape(int width, int height) {
+		point.translate(width, height);
+	}
 	
 	/**
 	 * 将图形的一元二次方程系数和常量设置为指定的系数和常量。
 	 * 使此图形的后续图形操作都使用此指定的系数和常量。
 	 */
-	public void setCoefficient() {
-		width = e_point.x - b_point.x;
-		height = e_point.y - b_point.y;
-		setCoefficient(width, height);
-	}
-	
-	protected void setCoefficient(Float width, Float height) {
+	private void setCoefficient() {
 		if(width == 0) {
 			coordinate = 1;
 			direction = height > 0 ? 1 : -1;
@@ -173,7 +191,7 @@ public abstract class BaseShape implements java.io.Serializable {
 			direction = width > 0 ? 1 : -1;
 		} else {
 			k = height / width;
-			b = b_point.y - k * b_point.x;
+			b = point.y - k * point.x;
 		}
 		System.out.println("width:" + width
 				+ ", height:" + height
